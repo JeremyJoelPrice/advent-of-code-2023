92c016abd52d6f952285c23d9844923d5c6d5928
Jeremy Joel Price <JeremyJoelPrice@gmail.com>
2023-12-17 23:21:58 +0000

    setup day 2

---
+++ src/main/java/com/jeremyjoelprice/Day2.java
@@ -0,0 +1,14 @@
+package com.jeremyjoelprice;
+
+public class Day2 extends Solution {
+	
+	@Override
+	String first(String input) {
+		return null;
+	}
+	
+	@Override
+	String second(String input) {
+		return null;
+	}
+}
---
+++ src/test/java/com/jeremyjoelprice/Day2Test.java
@@ -0,0 +1,17 @@
+package com.jeremyjoelprice;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Day2Test {
+	Solution solution = new Day2();
+	
+	@Nested
+	@DisplayName("Solution 1")
+	class Solution1 {
+	
+	}
+}
