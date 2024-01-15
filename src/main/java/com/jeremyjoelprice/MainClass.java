29ae8244399ae493d9078996abad76674d23b790
Jeremy Joel Price <JeremyJoelPrice@gmail.com>
2024-01-15 00:28:54 +0000

    refactor to expect only 1 puzzle input per day

--- src/main/java/com/jeremyjoelprice/MainClass.java
+++ src/main/java/com/jeremyjoelprice/MainClass.java
@@ -6,20 +6,18 @@ import java.nio.file.Path;
 
 public class MainClass {
 	public static void main(String[] args) {
-//		printSolution(new Day1(), "src/main/resources/puzzle-input/1-1.txt", "src/main/resources/puzzle-input/1-1.txt");
-//		printSolution(new Day2(), "src/main/resources/puzzle-input/2-1.txt", "src/main/resources/puzzle-input/2-1.txt");
-//		printSolution(new Day3(), "src/main/resources/puzzle-input/3-1.txt", "src/main/resources/puzzle-input/3-1.txt");
-		printSolution(new Day4(), "src/main/resources/puzzle-input/4-1.txt", null);
+		printSolution(new Day1(), "1");
+		printSolution(new Day2(), "2");
+		printSolution(new Day3(), "3");
+		printSolution(new Day4(), "4");
 	}
 	
-	private static void printSolution(Solution solution, String inputFilePathOne, String inputFilePathTwo) {
+	private static void printSolution(Solution solution, String inputFileName) {
 		try {
-			String contents = Files.readString(Path.of(inputFilePathOne));
-			System.out.println(solution.first(contents));
-			if (inputFilePathTwo != null) {
-				contents = Files.readString(Path.of(inputFilePathTwo));
-				System.out.println(solution.second(contents));
-			}
+			String puzzleInput = Files.readString(Path.of(String.format("src/main/resources/puzzle-input/%s.txt", inputFileName)));
+			System.out.printf("Day %s\n", inputFileName);
+			System.out.println("	Puzzle 1: " + solution.first(puzzleInput));
+			System.out.println("	Puzzle 2: " + solution.second(puzzleInput));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
