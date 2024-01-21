0a3174f847259308a7aca819310f4179889e0c5b
Jeremy Joel Price <JeremyJoelPrice@gmail.com>
2024-01-21 23:37:35 +0000

    solve day 5 puzzle 1

--- src/main/java/com/jeremyjoelprice/Day5.java
+++ src/main/java/com/jeremyjoelprice/Day5.java
@@ -1,13 +1,96 @@
 package com.jeremyjoelprice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Day5 extends Solution {
+	
 	@Override
 	String first(String input) {
-		return null;
+		List<String> almanacSections = new ArrayList<>(Arrays.asList(input.split("\n\n")));
+		
+		// parse seeds
+		long[] seeds = Arrays.stream(almanacSections.get(0)
+						.split(": ")[1]
+						.split(" "))
+				.mapToLong(Long::parseLong)
+				.toArray();
+		almanacSections.remove(0);
+		
+		
+		// parse each map and apply it to the seeds
+		for (String mapString : almanacSections) {
+			AlmanacMap map = getMapFromString(mapString);
+			
+			// pass each seed through all maps to get final results
+			for (int i = 0; i < seeds.length; i++) {
+				seeds[i] = map.transform(seeds[i]);
+			}
+		}
+		
+		// return lowest element of transformed seeds
+		long lowestNum = Arrays.stream(seeds).reduce(Long.MAX_VALUE, (a, b) -> Math.min(a, b));
+		return String.valueOf(lowestNum);
 	}
 	
 	@Override
 	String second(String input) {
 		return null;
+	}
+	
+	private AlmanacMap getMapFromString(String input) {
+		AdjustedRange[] ranges = Arrays.stream(input.split("\n"))
+				.skip(1)
+				.map((s) -> {
+					long[] rangeNums = Arrays.stream(s
+									.split(" "))
+							.mapToLong(Long::parseLong)
+							.toArray();
+					return new AdjustedRange(
+							rangeNums[0],
+							rangeNums[1],
+							rangeNums[2]);
+				}).toArray(size -> new AdjustedRange[size]);
+		
+		return new AlmanacMap(ranges);
+	}
+	
+	private class AdjustedRange {
+		long sourceStart;
+		long destinationStart;
+		long rangeSize;
+		
+		public AdjustedRange(long destinationStart, long sourceStart, long rangeSize) {
+			this.sourceStart = sourceStart;
+			this.destinationStart = destinationStart;
+			this.rangeSize = rangeSize;
+		}
+		
+		boolean isValid(long num) {
+			return num >= sourceStart && num < sourceStart + rangeSize;
+		}
+		
+		long transform(long num) {
+			// is num in range?
+			return num - (sourceStart - destinationStart);
+		}
+	}
+	
+	private class AlmanacMap {
+		AdjustedRange[] ranges;
+		
+		public AlmanacMap(AdjustedRange[] ranges) {
+			this.ranges = ranges;
+		}
+		
+		long transform(long num) {
+			for (AdjustedRange range : ranges) {
+				if (range.isValid(num)) {
+					return range.transform(num);
+				}
+			}
+			return num;
+		}
 	}
 }
--- src/test/java/com/jeremyjoelprice/Day5Test.java
+++ src/test/java/com/jeremyjoelprice/Day5Test.java
@@ -11,6 +11,138 @@ class Day5Test {
 	@Nested
 	@DisplayName("Solution 1")
 	class Solution1 {
+		@Test
+		@DisplayName("returns lowest seed when not filtered through any maps")
+		void returnsLowestSeedWhenNotFilteredThroughAnyMaps() {
+			assertAll(
+					() -> assertEquals("1", new Day5().first("seeds: 1 2 3"))
+					, () -> assertEquals("13", new Day5().first("seeds: 79 14 55 13"))
+			);
+		}
+		
+		@Test
+		@DisplayName("returns lowest number when passed through one map which lowers the value of a certain range")
+		void returnsLowestNumberWhenPassedThroughOneMapWhichLowersTheValueOfACertainRange() {
+			assertAll(
+					() -> assertEquals("10", new Day5().first("""
+							seeds: 10 20
+							
+							sample map:
+							50 90 1"""))
+					, () -> assertEquals("1", new Day5().first("""
+							seeds: 10 20
+							
+							sample map:
+							1 20 1"""))
+					, () -> assertEquals("2", new Day5().first("""
+							seeds: 10 20 30
+							
+							sample map:
+							1 29 3"""))
+			);
+		}
+		
+		@Test
+		@DisplayName("returns lowest number when passed through one map which raises the value of a certain range")
+		void returnsLowestNumberWhenPassedThroughOneMapWhichRaisesTheValueOfACertainRange() {
+			assertAll(
+					() -> assertEquals("20", new Day5().first("""
+							seeds: 10 20
+							
+							sample map:
+							50 10 1"""))
+					, () -> assertEquals("10", new Day5().first("""
+							seeds: 10 20
+							
+							sample map:
+							22 20 1"""))
+					, () -> assertEquals("20", new Day5().first("""
+							seeds: 10 20 30
+							
+							sample map:
+							50 9 3"""))
+			);
+		}
+		
+		@Test
+		@DisplayName("returns lowest number when passed through one map with multiple ranges")
+		void returnsLowestNumberWhenPassedThroughOneMapWithMultipleRanges() {
+			assertAll(
+					() -> assertEquals("1", new Day5().first("""
+							seeds: 10 20 30
+							
+							sample map:
+							50 10 1
+							1 30 1"""))
+					, () -> assertEquals("20", new Day5().first("""
+							seeds: 10 20 30
+							
+							sample map:
+							50 10 1
+							60 20 1
+							20 30 1
+							1 60 1"""))
+			);
+		}
+		
+		@Test
+		@DisplayName("returns lowest number when passed through multiple maps")
+		void returnsLowestNumberWhenPassedThroughMultipleMaps() {
+			assertAll(
+					() -> assertEquals("1", new Day5().first("""
+							seeds: 10 20 30
+							
+							map:
+							2 20 1
+							
+							map:
+							1 30 1
+							"""))
+					, () -> assertEquals("49", new Day5().first("""
+							seeds: 53
+							
+							fertilizer-to-water map:
+							49 53 8
+							0 11 42
+							"""))
+					, () -> assertEquals("43", new Day5().first("""
+							seeds: 14
+							
+							seed-to-soil map:
+							50 98 2
+							52 50 48
+							
+							soil-to-fertilizer map:
+							0 15 37
+							37 52 2
+							39 0 15
+							
+							fertilizer-to-water map:
+							49 53 8
+							0 11 42
+							42 0 7
+							57 7 4
+							
+							water-to-light map:
+							88 18 7
+							18 25 70
+							
+							light-to-temperature map:
+							45 77 23
+							81 45 19
+							68 64 13
+							
+							temperature-to-humidity map:
+							0 69 1
+							1 0 69
+							
+							humidity-to-location map:
+							60 56 37
+							56 93 4
+							"""))
+			);
+		}
+		
 	}
 	
 	@Nested
